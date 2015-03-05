3ba7a1945dc1bab1d5b94087bdb8a30cbc3e3dc3
MKS-Student <mks.student@hackreactor.com>
2015-03-05 09:49:52 -0800

    Tests pass for all four patterns.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.back = 0;
+  this.front = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+	this.storage[this.back] = value;
+	this.back++;
+};
 
+Queue.prototype.dequeue = function() {
+	if(this.back > this.front) {
+		var result = this.storage[this.front];
+		delete this.storage[this.front];
+		this.front++;
+		return result;
+	}
+};
+
+Queue.prototype.size = function() {
+	return this.back - this.front;
+};
