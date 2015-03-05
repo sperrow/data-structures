27b6d7dd378f571d56aea2b3898b3c0c32fd9151
MKS-Student <mks.student@hackreactor.com>
2015-03-04 16:42:16 -0800

    All tests pass for functional pattern. Stack passes in functional-shared pattern.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+ 	  storage : {},
+	  stackSize : 0
+  };
+
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+	push: function(value){
+		this.storage[this.stackSize] = value;
+		this.stackSize++;
+	},
+	pop: function(){
+		if(this.stackSize > 0) {
+			this.stackSize--;
+			var result = this.storage[this.stackSize];
+			delete this.storage[this.stackSize];
+			return result;
+		}
+	},
+	size: function(){
+		return this.stackSize;
+	}
+};
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,27 @@ var Queue = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var back = 0;
+  var front = 0;
+
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[back] = value;
+    back++;
   };
 
   someInstance.dequeue = function(){
+    if(back > front) {
+      var result = storage[front];
+      delete storage[front];
+      front++;
+      return result;
+    }
   };
 
   someInstance.size = function(){
+    return back - front;
   };
 
   return someInstance;
