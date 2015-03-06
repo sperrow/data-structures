cb7527bc13f2289a11a15c100ed38f8cc7135903
MKS-Student <mks.student@hackreactor.com>
2015-03-06 12:34:54 -0800

    Tests pass for set.js and hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,37 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var list = [];
+  if(this._storage.get(i)) {
+  	list = this._storage.get(i);
+  }
+  list.push([k, v]);
+  this._storage.set(i, list);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var list = this._storage.get(i);
+  if(list){
+  	for (var j = 0 ; j<list.length ; j++){
+  		if (list[j][0] === k){
+  			return list[j][1];;
+  		}
+  	}
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
-
+	var i = getIndexBelowMaxForKey(k, this._limit);
+	var list = this._storage.get(i);
+	if(list){
+		for (var j = 0 ; j<list.length ; j++){
+  		if (list[j][0] === k){
+  			list.splice(j,1);
+  		}
+  	}
+	}
 };
 
 
