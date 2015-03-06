99818ac433cb16188d0ed20462e8e7e1d86ecf61
MKS-Student <mks.student@hackreactor.com>
2015-03-06 15:53:28 -0800

    Tests pass for binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,60 @@
 var BinarySearchTree = function(value){
+	var newTree = {};
+
+	newTree.value = value;
+	newTree.left;
+	newTree.right;
+
+	newTree.insert = function(value) {
+		if(!this.value) {
+			this.value = value;
+		} else if(value < this.value) {
+			if(this.left) {
+				this.left.insert(value);
+			} else {
+				this.left = BinarySearchTree(value);
+			}
+		} else if(value > this.value) {
+			if(this.right) {
+				this.right.insert(value);
+			} else {
+				this.right = BinarySearchTree(value);
+			}
+		}
+	};
+
+	newTree.contains = function(value) {
+		if(value === this.value) {
+			return true;
+		} else if(value < this.value) {
+			if(this.left) {
+				if(this.left.contains(value)) {
+					return true;
+				}
+			}
+		} else if(value > this.value) {
+			if(this.right) {
+				if(this.right.contains(value)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	};
+
+	newTree.depthFirstLog = function(cb) {
+		if(this.value) {
+			cb(value);
+		}
+		if(this.left) {
+			this.left.depthFirstLog(cb);
+		}
+		if(this.right) {
+			this.right.depthFirstLog(cb);
+		}
+	};
+
+	return newTree;
 };
 
 
