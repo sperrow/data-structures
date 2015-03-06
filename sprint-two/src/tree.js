7d1666a6ef9b769c68e42cb9e48df5f4b529cbd9
MKS-Student <mks.student@hackreactor.com>
2015-03-05 16:09:38 -0800

    Tests pass for tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -15,11 +17,26 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+	var tree = Tree(value);
+	this.children.push(tree);
 };
 
 treeMethods.contains = function(target){
+	var result = false;
 
+	var recurse = function(tree, target) {
+		if(tree.value === target) {
+			result = true;
+		} else if(tree.children.length > 0) {
+			for(var i = 0; i < tree.children.length; i++) {
+				recurse(tree.children[i], target);
+			}
+		}
+	};
+
+	recurse(this, target);
+
+	return result;
 };
 
 
