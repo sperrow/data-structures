8534dc312d392ca26babb20b8717866bc05df366
MKS-Student <mks.student@hackreactor.com>
2015-03-06 16:05:50 -0800

    Refactor set.js to run in constant time.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,30 +1,42 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = [];
+  set._storage = new HashTable();
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
-	var index = this._storage.indexOf(item);
-	if(index < 0){
-		this._storage.push(item);
-	}
+	this._storage.insert(item, item);
+
+	// *original solution*
+	// var index = this._storage.indexOf(item);
+	// if(index < 0){
+	// 	this._storage.push(item);
+	// }
 };
 
 setPrototype.contains = function(item){
-	if (this._storage.indexOf(item) >= 0){
+	if(this._storage.retrieve(item)) {
 		return true;
 	}
 	return false;
+
+	// *original solution*
+	// if (this._storage.indexOf(item) >= 0){
+	// 	return true;
+	// }
+	// return false;
 };
 
 setPrototype.remove = function(item){
-	var index = this._storage.indexOf(item);
-	if(index >=0){
-		this._storage.splice(index, 1);
-	}
+	this._storage.remove(item);
+
+	// *original solution*
+	// var index = this._storage.indexOf(item);
+	// if(index >=0){
+	// 	this._storage.splice(index, 1);
+	// }
 };
 
 /*
